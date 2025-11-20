2af86c2535241285e19797dd48501a9bd2d9649e
Ben Bahrman <benbahrman@gmail.com>
2025-11-19 20:45:01 -0600

    SPLIT_LED_STATE_ENABLE allows caps lock status to be shown on  secondary side

--- keyboards/crkbd/keymaps/ben_corne_42/config.h
+++ keyboards/crkbd/keymaps/ben_corne_42/config.h
@@ -26,6 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define TAPPING_TERM 100
 #define SPLIT_OLED_ENABLE
 #define SPLIT_LAYER_STATE_ENABLE
+#define SPLIT_LED_STATE_ENABLE
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
     #define RGBLIGHT_EFFECT_RAINBOW_MOOD
