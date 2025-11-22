1fb3d52304007866325e7331f71cf8399a541fbe
Ben Bahrman <benbahrman@gmail.com>
2025-11-22 13:57:11 -0600

    Stripped out screens

--- keyboards/crkbd/keymaps/ben_corne_42/config.h
+++ keyboards/crkbd/keymaps/ben_corne_42/config.h
@@ -24,7 +24,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //#define QUICK_TAP_TERM 0
 //#define TAPPING_TERM 100
-#define SPLIT_OLED_ENABLE
 #define SPLIT_LAYER_STATE_ENABLE
 #define SPLIT_LED_STATE_ENABLE
 #ifdef RGBLIGHT_ENABLE
